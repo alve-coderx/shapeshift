409e5e7152344b5b7902bc28f504dace5ae672ed
Alve <shadmanzz656@gmail.com>
2023-06-02 23:23:52 -0600

    ui updated 04

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Hero from "./Components/Hero";
 import videoAs from "./Assets/Apex_1685768084262.webm";
 const App = () => {
   return (
-    <div className="bgvdo">
-      <video
-        className="lg:object-cover object-none"
-        autoPlay={true}
-        loop
-        muted
-        controls={false}
-      >
-        <source src={videoAs} type="video/mp4" />
-      </video>
-      <Hero />
+    <div>
+      <div className="bgvdo lg:block md:block hidden">
+        <video
+          className="object-cover lg:block md:block hidden"
+          autoPlay={true}
+          loop
+          muted
+          controls={false}
+        >
+          <source src={videoAs} type="video/mp4" />
+        </video>
+        <Hero />
+      </div>
+      <div className="mbbg lg:hidden md:hidden block">
+        <Hero />
+      </div>
     </div>
   );
 };

5ece97adf9fd4772372f088792bbd4cff439ebf1
Alve <shadmanzz656@gmail.com>
2023-06-02 23:04:10 -0600

    ui updated 03

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import videoAs from "./Assets/Apex_1685768084262.webm";
 const App = () => {
   return (
     <div className="bgvdo">
-      <video autoPlay loop muted>
+      <video autoPlay loop muted controls={false}>
         <source src={videoAs} type="video/mp4"/>
       </video>
       <Hero />
